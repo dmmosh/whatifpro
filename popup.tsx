9b228ebae952f7a074f3b5e12ca54fe5b8226d5c
Anahel <bour.anahel14@gmail.com>
2025-10-25 01:32:54 -0400

    changes

--- popup.tsx
+++ popup.tsx
@@ -1,30 +1,19 @@
-
-//import functionName from "./src/function.tsx"
-import { useState, useEffect } from "react"
 import type { JSX } from "react"
+// 1. Import your new custom hook
+import { useCurrentUrl } from "./src/hooks/useCurrentUrl"
 
 function IndexPopup(): JSX.Element { 
-  const [currentUrl, setCurrentUrl] = useState<string>("")
+  // 2. Use the hook to get the data. All the complex logic is hidden away.
+  const currentUrl = useCurrentUrl()
 
-  const getCurrentUrl = async () => {
-    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
-    
-    if (tab && tab.url) {
-      setCurrentUrl(tab.url)
-    }
-  }
-
-  useEffect(() => {
-    getCurrentUrl()
-  }, [])
-
+  // 3. The return (view) is all that's left
   return (
     <div
       style={{
         padding: 16
       }}>
       <h1>You are currently at:</h1>
-      <p>{currentUrl}</p>
+      <p>{currentUrl || "Loading..."}</p> {/* Added a loading state */}
     </div>
   )
 }
